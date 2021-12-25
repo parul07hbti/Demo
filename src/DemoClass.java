066cf74170804bbe349a3afdeaaa09c728a7024b
bn <parul07hbti>
2021-12-25 14:47:15 +0530

    demo

---
+++ src/DemoClass.java
@@ -0,0 +1,8 @@
+public class DemoClass {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Java");
+        System.out.println("Welcome to the Spring");
+        System.out.println("Welcome to the Spring boot");
+    }
+}
